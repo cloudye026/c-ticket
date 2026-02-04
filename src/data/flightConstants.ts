517d7faca8327efabff1e846bfd16784dab5e2e9
cloudye026 <79581129+cloudye026@users.noreply.github.com>
2026-02-04 16:32:06 +0800

    Update flightConstants.ts

--- src/data/flightConstants.ts
+++ src/data/flightConstants.ts
@@ -14,21 +14,10 @@ export const terminalOptions = [
   { value: "T5", label: "T5" },
   { value: "T3A", label: "T3A" },
   { value: "大兴机场", label: "大兴机场" },
-  { value: "虹桥T1", label: "虹桥T1" },
-  { value: "虹桥T2", label: "虹桥T2" },
-  { value: "浦东T1", label: "浦东T1" },
-  { value: "浦东T2", label: "浦东T2" },
-  { value: "白云T1", label: "白云T1" },
-  { value: "白云T2", label: "白云T2" },
-  { value: "宝安T3", label: "宝安T3" },
-  { value: "双流T1", label: "双流T1" },
-  { value: "双流T2", label: "双流T2" },
-  { value: "天河T1", label: "天河T1" },
-  { value: "天河T2", label: "天河T2" },
+  { value: "I", label: "I" },
+  { value: "D", label: "D" },
   { value: "长水", label: "长水" },
-  { value: "咸阳T1", label: "咸阳T1" },
-  { value: "咸阳T2", label: "咸阳T2" },
-  { value: "咸阳T3", label: "咸阳T3" },
+ 
 ];
 
 // 托运行李选项
